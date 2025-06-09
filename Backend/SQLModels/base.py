77dd64b8c19b22a8a1941c9a0c16f2ce5a538492
Zheng Feng <cheahzhengfeng@gmail.com>
2025-06-10 01:07:57 +0800

    connection to DB through sqlalchemy

--- Backend/SQLModels/base.py
+++ Backend/SQLModels/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy.orm import sessionmaker
 import os 
@@ -28,4 +28,43 @@ def initDatabase():
         dbHost = "127.0.0.1"
         dbPort = sshTunnel.local_bind_port 
 
-        print(f"→ SSH tunnel active: localhost:{dbPort} → EC2:3306")
+        print(f"→ SSH tunnel active: localhost:{dbPort} → EC2:{os.getenv("MYSQL_REMOTE_PORT")}")
+
+    else : 
+        # connect locally to the db
+        dbHost = os.getenv("MYSQL_CONTAINER_NAME")
+        dbPort = int(os.getenv("MYSQL_CONTAINER_PORT"))  
+    
+
+    # the db user credentials  
+    dbUser = os.getenv("MYSQL_USER") 
+    dbPassword = os.getenv("MYSQL_PASSWORD") 
+    dbName = os.getenv("MYSQL_DATABASE") 
+
+
+    # build the URL / connection string
+
+    databaseURL = f"mysql+pymysql://{dbUser}:{dbPassword}@{dbHost}:{dbPort}/{dbName}" 
+    print(f"-> SQLAlchemy connecting to {databaseURL}") 
+
+    #create the engine 
+    engine = create_engine(databaseURL, echo=True, pool_pre_ping=True) 
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
+
+
+    # test connection 
+
+    try : 
+        with engine.connect() as connection:
+            print ("→ Testing database connection...") 
+            result = connection.execute(text("SHOW TABLES;")) 
+            
+            print("→ Database connection established successfully.") 
+            return result.fetchall()  # Return the list of tables if needed 
+    except: 
+        print("→ Failed to connect to the database. Please check your connection settings.") 
+        if useSSH:
+            # sshTunnel.stop() 
+
+            #print("→ SSH tunnel closed due to connection failure.") 
+            print (f" -> SSH tunnel is still active on localhost:{dbPort}")
--- Backend/app.py
+++ Backend/app.py
@@ -4,7 +4,7 @@ import os
 from db import sshFlow, noSshFlow
 app = Flask(__name__) 
 import dotenv
-
+from SQLModels.base import initDatabase
 
 # allow all domains to access the API 
 CORS(app)
@@ -23,7 +23,7 @@ def hello():
 
 @app.route('/test')
 def test(): 
-    # useSSH = os.environ.get("USE_SSH_TUNNEL", "False").lower() in ("1", "true", "yes")
+    # useSSH = os.environ.get("USE_SSH_TUNNEL",6 "False").lower() in ("1", "true", "yes")
     useSSH = os.environ.get("USE_SSH_TUNNEL") in ("1", "true", "yes") 
     if useSSH:
         print ("ssh is turned on")
@@ -33,10 +33,20 @@ def test():
         print ("ssh is turned off") 
         ans = noSshFlow() 
         return jsonify({"message": "database records fetched from API! no ssh", "status": 169, "ans": ans, "useSSH": useSSH})  
+
+@app.route('/initDB')
+def init_db():
     
+    tables = initDatabase()
+    print (f"Tables in the database: {tables}") 
+    return jsonify({"message": "Database initialized successfully!",
+                    "tables": [str(table[0]) for table in tables]})  
         
 
 if __name__  == "__main__":
+    #! for tssting purposes , we load the .env file and .env.dev 
+    dotenv.load_dotenv('.env')
+    dotenv.load_dotenv('.env.dev')  
     # Read host, port, and debug from env, with sensible defaults
     host  = os.environ.get("FLASK_RUN_HOST")
     port  = int(os.environ.get("FLASK_RUN_PORT"))
