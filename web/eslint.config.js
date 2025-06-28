5cb142b38251a1e95a3eb656c096dd11a58e1e34
Charlene <72479396+jihoonieeee@users.noreply.github.com>
2025-06-28 14:06:13 +0800

    Static analysis eslint flake8 and bandit

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,4 +1,3 @@
-import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
@@ -10,7 +9,7 @@ import pluginNoUnsanitized from "eslint-plugin-no-unsanitized";
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
