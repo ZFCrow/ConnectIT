be084bd2d030967010d2d374b658edbbd65d1978
Jake Wong <jake_wongkw@hotmail.com>
2025-07-04 22:01:34 +0800

    solve flake8

--- Backend/Routes/auth.py
+++ Backend/Routes/auth.py
@@ -33,7 +33,7 @@ def register():
 
     if payload["role"] != "User" or "Company":
         return jsonify({"error": "Failed to create account"}), 500
-    
+
     if errors:
 
         SplunkLogging.send_log(
